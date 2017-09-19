b328b230919e2b32ad381dafc1c01b0a648673f7
icer3 <icer3@icer1s-MacBook-Pro.local>
2017-09-19 11:05:46 -0400

    update

--- RumpsExample.py
+++ RumpsExample.py
@@ -1,4 +1,5 @@
 from urllib import urlretrieve
+import os.path
 import rumps
 import subprocess
 import os
@@ -6,6 +7,8 @@ import pipes
 import glob
 import pexpect
 import getpass
+import sys
+import webbrowser
 
 class StatusBarApp(rumps.App):
 
@@ -13,24 +16,47 @@ class StatusBarApp(rumps.App):
 
     def __init__(self):
         super(StatusBarApp, self).__init__("SSHFS") # refers to parent base class without explicitly calling their intits methods
-        self.menu = [rumps.MenuItem('Mount HPCC',callback=self.mount,key='q'),rumps.MenuItem('Add Host',key='a'),rumps.MenuItem('Unmount',callback=self.unmount)]
+        self.menu = [rumps.MenuItem('Mount HPCC',callback=self.mount,key='q'),rumps.MenuItem('Unmount',callback=self.unmount),
+                    {
+                        "More info":{"ICER","SSH KEYS"}
+                    }
+                    ]
+        
         self.icon = 'icer.png'
         self.local_mount_point = "/Volumes"
         self.path = "/HPCC"
         self.login = getpass.getuser()
-        rumps.debug_mode(True)          # Turn on debugging mode while editiing
+        rumps.debug_mode(True)
 
     def check_programs(self):
-        pass
-    
-    def key_present(self):              # checks to see if file
-        CUR_USR = "/Users/" + self.login + "/.ssh"
-        return ("id_rsa" in os.listdir(CUR_USR))
+        try:
+            subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.MacFUSE", shell=True)
+            subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.SSHFS", shell=True)
+            return True
+        except subprocess.CalledProcessError:
+            #self.error_msg = rumps.alert(title="Software requirements",
+            #                             message="Your computer is missing some software requirements to mount your HPCC file system with SSHFS. Please click on the link and install the necessary packages.")
+            return False
 
-    def gen_key(self):                  # generate the keys
-        home = os.path.expanduser('~')
-        subprocess.call('ssh-keygen -t rsa -q -f ' + home + '/.ssh/id_rsa ', shell=True)
-                                        # ssh - keygen - t rsa - q - f "$HOME/.ssh/id_rsa" - N""
+    def key_present(self,userid):
+        # Batchmode=yes and password less connectivity is enable the command execute successfully on remote, else it will return error and continues.
+        var = subprocess.check_output(
+            ["ssh -o 'BatchMode=yes' " + userid + "@hpcc.msu.edu 'echo 2>&1' && echo 'OK' || echo 'NOK' "], shell=True)
+        if var.strip() == "OK":
+            return True
+        return False
+
+    def gen_key(self):
+        #home = os.path.expanduser('~')
+        if (os.path.isfile("~/Users/icer3/.ssh/id_rsa") == False):
+            self.ssh_passphrase = rumps.Window(title="RSA private key passphrase",message="Enter passphrase (empty for no passphrase:)",secure=True,dimensions=(250,50)).run()
+            if not self.ssh_passphrase.text:
+                subprocess.call("ssh-keygen -t rsa -q -f  ~/.ssh/id_rsa", shell=True)     # changed this to hpcc
+            else:
+                subprocess.call("ssh-keygen -t rsa -N " + self.ssh_passphrase.text + " -f ~/.ssh/id_rsa", shell=True)      # changed this to hpcc
+            # ssh - keygen - t rsa - q - f "$HOME/.ssh/id_rsa" - N""
+        else:
+            print "No File is present"
 
     def exists_remote(self, userid):
         """Test if a file exists at path on a host accessible with SSH."""
@@ -42,16 +68,37 @@ class StatusBarApp(rumps.App):
             return False
         raise Exception('SSH failed')
 
-    def push_key(self, userid): # copy over to the ssh
+    def push_key(self, userid):
         path = os.chdir("/Users/" + self.login + "/.ssh")
         Files = glob.glob1(path, '*.pub')
         public_id = Files[0]
-        child = pexpect.spawn('scp -o StrictHostKeyChecking=no ' + "/Users/" + self.login + "/.ssh/" + public_id + " " + userid + "@hpcc.msu.edu:.ssh/authorized_keys")
-        child.expect_exact('Password:')
-        self.Window = rumps.Window("Please enter your HPCC password:",dimensions=(150,50)).run()
-        mypassword = str(self.Window.text)
+        print public_id
+        child = pexpect.spawn("scp -o StrictHostKeyChecking=no " + "/Users/" + self.login + "/.ssh/" + public_id + " " + userid + "@hpcc.msu.edu:.ssh/authorized_keys")
+        #print '/bin/cat ~/.ssh/id_rsa.pub | ssh ' + userid + '@hpcc.msu.edu -o StrictHostKeyChecking=no "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"'
+        #child = pexpect.spawn("cat ~/.ssh/id_rsa.pub | ssh " + userid + "@hpcc.msu.edu -o StrictHostKeyChecking=no 'cat >> ~/.ssh/authorized_keys'")
+        child.expect('Password:')
+        #child.expect(pexpect.EOF, timeout=None)
+        #print child.expect(pexpect.EOF)
+        self.HPCC_PASS = rumps.Window(title="HPCC Password",message="Please enter your HPCC password:",dimensions=(250,50),secure=True).run()
+        mypassword = str(self.HPCC_PASS.text)
         child.sendline(mypassword)
-        child.expect(pexpect.EOF, timeout=None)
+
+        Max_attempts = 3
+        Attempt = 1
+        while True:
+            result = child.expect(['Password:', pexpect.EOF, pexpect.TIMEOUT])
+            print result, "result"
+            if result == 0 and Max_attempts >= 1:
+                self.HPCC_PASS = rumps.Window(title="Incorrect HPCC password.   Attempt: (""/3)", message="Please re-enter your HPCC password:",
+                                          dimensions=(250, 50), secure=True).run()
+                mypassword = str(self.HPCC_PASS.text)
+                child.sendline(mypassword)
+            elif Max_attempts < 1:
+                self.message = rumps.alert(title="Incorrect HPCC password and userID",message="Please check your that you entered your HPCC username or Password Correctly.")
+                sys.exit(0)
+            else:
+                break
+            Max_attempts -= 1
 
     def drive_mounted(self):
         return (os.path.exists(self.local_mount_point + self.path))
@@ -61,40 +108,44 @@ class StatusBarApp(rumps.App):
 
         print(self.login)
 
-        self.Window = rumps.Window("Please enter your MSU NETID", dimensions=(250, 50)).run()  # Holds information from user interaction with a rumps after it has been closed
+        self.UID = rumps.Window(title="HPCC username",message="Please enter your MSU NETID:", dimensions=(250, 50)).run()
 
-        UserID = str(self.Window.text)  # since rumps.Window returns its type from window class we need to convert it to string
+        UserID = str(self.UID.text)  # since rumps.Window returns its type from window class we need to convert it to string
 
-        if (self.key_present()):
-            print ("The SSH keys are present {}".format(self.key_present()))
+        if (self.key_present(UserID)): # SSH does not connect through
+            print ("The SSH keys are present {}".format(self.key_present(UserID)))
         else:
             print "Need to generate ssh keys"
-            self.alert = rumps.alert(message="To mount your HPCC drive using SSHFS, you need to have SSH Key-Based authentication. Would you like to"
-                                             "continue to create your SSH Keys. ")
+
+            self.alert = rumps.alert(title="SSH Keys",
+                                     message="To mount your HPCC drive using SSHFS, you need to generate SSH Key-Based "
+                                             "authentication. Would you like to continue to create your SSH Keys. NOTE: "
+                                             "By clicking continue, the program will generate two files onto your computer "
+                                             "system, a public and a private key. Additionally, the public key will be copied "
+                                             "to your HPCC system account to a file within the your home directory at ~/.ssh/authorized_keys."
+                                             " This procedure will enable Password-less login when connecting to the HPCC. This way, you'll "
+                                             "be able to automatically login each time immediately without needing to enter your password. ",
+                                     ok="Continue", cancel="Cancel")
+
+            if self.alert == 0:
+                    sys.exit(0)
             self.gen_key()
             self.push_key(UserID)
 
-        #bool = self.exists_remote(UserID)
-        #if(bool):
-        #   print("Authorized_keys exist")
-
-        #os.chdir("/Users/" + self.login + "/.ssh")
-        #cwd = os.getcwd()
-
         sshfs_cmd = "/usr/local/bin/sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa"
-        remote_host = "@rsync.hpcc.msu.edu:/mnt/home/"
-        options = "-o cache=no -o nolocalcaches -o volname=hpcc -o StrictHostKeyChecking=no"   # StrictHostKeyChecking ignores authentication for first time use
+        remote_host = "@hpcc.msu.edu:/mnt/home/"
+        options = "-o cache=no -o nolocalcaches -o volname=hpcc -o StrictHostKeyChecking=no"
+        # StrictHostKeyChecking ignores authentication for first time use
 
         os.mkdir(self.local_mount_point + self.path)
         var = sshfs_cmd + " " + UserID + remote_host + UserID +  " " + self.local_mount_point + self.path + " " + options
 
         print(var)
         os.system(var)
-
-        if (self.drive_mounted()):                                                    # os.system() returns the encoded process exit value. 0 means succecss no error
+        if (self.drive_mounted()):     # os.system() returns the encoded process exit value. 0 means succecss no error
              self.menu["Mount HPCC"].set_callback(None)
-             rumps.notification(title="HPCC drive Mounted 1", subtitle="Sucessfull",  # !!!!Fix This
-                               message="Your drive is located at " + self.local_mount_point+self.path)
+             rumps.notification(title="HPCC drive Mounted", subtitle="Sucessfull",  # !!!!Fix This
+                               message="Your drive is located at " + self.local_mount_point + self.path,sound=True)
 
     @rumps.clicked("Unmount")
     def unmount(self, _):
@@ -102,12 +153,12 @@ class StatusBarApp(rumps.App):
             subprocess.call("diskutil umount" + " " +  self.local_mount_point + self.path, shell = True)
             self.menu["Mount HPCC"].set_callback(self.mount)
         else:
-            self.alert = rumps.alert(message="Your HPCC drive is not mounted")
+            self.alert = rumps.alert(title = "HPCC drive not currently mounted",message="Your HPCC drive is not mounted")
 
-
-    @rumps.clicked("Add Host")
-    def add_host(self, _):
-        pass
+    @rumps.clicked("More info")
+    def more_info(self,_):
+        b = webbrowser.get('safari')
+        b.open("https://icer.msu.edu")
 
 if __name__ == "__main__":
     StatusBarApp().run()
--- decorators.py
+++ decorators.py
@@ -1,9 +1,79 @@
 # Decorators
 import getpass
 import os
+
 username = getpass.getuser()
 print username
+import sys
+import subprocess
+import rumps
+import pexpect
 
-import os
-homedir = os.environ['HOME']
-print homedir
+# var = subprocess.check_output(["ssh -o 'BatchMode=yes' zahidmuh@hpcc.msu.edu 'echo 2>&1' && echo 'OK' || echo 'NOK' "],shell=True)
+
+
+# p = subprocess.Popen(['open', '-a', 'Terminal', '-n'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+# output = p.communicate(input = "ssh-keygen -t rsa")
+#
+# print(output)
+
+
+
+
+#
+# password = rumps.Window(secure=True).run()
+#
+#
+# if not password.text:
+#     print "No password entered"
+# else:
+#     print ("Password: {}".format(password.text))
+
+
+cond = True
+
+
+# while cond:
+#     if 2 == 2 :
+#         print "here"
+#         break
+#     else:
+#         print "kobe"
+
+
+def test():
+    try:
+        subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.MacFUSE", shell=True)
+        subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.SSHFS", shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+        # error_msg = rumps.alert(title="Software requirements",
+        # message="You are missing somesoftware requirements. Please click on the link and install the necessary packages."
+
+
+cur_usr = "/Users/" + "icer3" + "/.ssh"
+print cur_usr
+if ("id_rsa_hpcc" in os.listdir('/Users/icer3/.ssh')):
+    print "It exists"
+
+
+# try:
+#     subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.MacFUSE",shell=True)
+#     subprocess.check_call("pkgutil --pkg-info com.github.osxfuse.pkg.SSHFS1", shell = True)
+# except subprocess.CalledProcessError:
+#     print "One of the programs dont exust"
+#     alert = rumps.alert(title="SSH Keys",
+#                             message="To mount your HPCC drive using SSHFS, you need to have SSH Key-Based "
+#                                     "authentication. Would you like to continue to create your SSH Keys."
+#
+#
+#
+#                                     "For more Info",
+#                             ok="Continue", cancel="Cancel")
+#
+#     if alert == 1 :
+#         print("conTinued")
+#     else:
+#         print ("Canceled")
